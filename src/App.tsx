72b0ecee9139b3d545e4ca6a7d0cbbfd60b02065
Maxvo <maxvo25@gmail.com>
2025-11-29 08:08:13 +1030

    fix text

--- src/App.tsx
+++ src/App.tsx
@@ -27,10 +27,6 @@ export default function App(): JSX.Element {
       }
     };
 
-    (window as any).gotomars = (to: string) => {
-      console.log('Going to mars');
-    };
-
     return () => {
       window.removeEventListener('popstate', onPop);
       delete (window as any).__navigate;
